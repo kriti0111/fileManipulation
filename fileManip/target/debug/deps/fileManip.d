32a63e62993d89497bd8f8fdce1a372002edb376
Kriti Tulsian <kriti.tulsian96@gmail.com>
2020-05-20 13:11:40 +0800

    Rectified file structure

---
+++ fileManip/target/debug/deps/fileManip.d
@@ -0,0 +1,5 @@
+C:\Users\dell\projects\fileManip\target\debug\deps\fileManip.exe: src\main.rs
+
+C:\Users\dell\projects\fileManip\target\debug\deps\fileManip.d: src\main.rs
+
+src\main.rs:
---
+++ fileManip/target/debug/fileManip.d
@@ -0,0 +1,1 @@
+C:\Users\dell\projects\fileManip\target\debug\fileManip.exe: C:\Users\dell\projects\fileManip\src\main.rs
